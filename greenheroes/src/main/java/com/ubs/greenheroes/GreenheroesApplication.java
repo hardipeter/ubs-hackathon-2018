f0660adbf900641fb8da50b7a6d8b0671a3023b7
Daniel Porazik <porazik@gmail.com>
2018-09-06 19:23:47 +0200

    Added reading of instrument names from file

--- greenheroes/src/main/java/com/ubs/greenheroes/GreenheroesApplication.java
+++ greenheroes/src/main/java/com/ubs/greenheroes/GreenheroesApplication.java
@@ -4,10 +4,12 @@ import com.ubs.greenheroes.generate.MockedDataGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileNotFoundException;
+
 @SpringBootApplication
 public class GreenheroesApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         MockedDataGenerator.createMockedInstruments();
         MockedDataGenerator.createMockedClient();
         SpringApplication.run(GreenheroesApplication.class, args);
