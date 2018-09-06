d078c4f3d2c456eb29c9a88fdf04036820286062
Peter Hardi <hardipeter@10.34.212.247>
2018-09-06 18:44:29 +0200

    cors setup

--- greenheroes/src/main/java/com/ubs/greenheroes/controllers/ClientPreferenceController.java
+++ greenheroes/src/main/java/com/ubs/greenheroes/controllers/ClientPreferenceController.java
@@ -1,9 +1,9 @@
 package com.ubs.greenheroes.controllers;
 
-import com.ubs.greenheroes.data.MockedDatabase;
 import com.ubs.greenheroes.data.Client;
 import com.ubs.greenheroes.data.ClientPreference;
 import com.ubs.greenheroes.data.InterestLevel;
+import com.ubs.greenheroes.data.MockedDatabase;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientPreferenceController {
 
-    private static final String ACCEPTED_ORIGINS = "http://localhost:3000";
+    static final String ACCEPTED_ORIGINS = "http://localhost:3000";
 
     @CrossOrigin(origins = ACCEPTED_ORIGINS)
     @RequestMapping("/preferences")
@@ -21,6 +21,7 @@ public class ClientPreferenceController {
         return MockedDatabase.CLIENT;
     }
 
+    @CrossOrigin(origins = ACCEPTED_ORIGINS)
     @RequestMapping("/preferences/{id}")
     public void setClientPreference(@PathVariable(name = "id") int id,
                                     @RequestBody InterestLevel interest) {
--- greenheroes/src/main/java/com/ubs/greenheroes/controllers/InstrumentController.java
+++ greenheroes/src/main/java/com/ubs/greenheroes/controllers/InstrumentController.java
@@ -1,14 +1,17 @@
 package com.ubs.greenheroes.controllers;
 
-import com.ubs.greenheroes.data.MockedDatabase;
+import static com.ubs.greenheroes.controllers.ClientPreferenceController.ACCEPTED_ORIGINS;
 import com.ubs.greenheroes.data.Instrument;
+import com.ubs.greenheroes.data.MockedDatabase;
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InstrumentController {
 
+    @CrossOrigin(origins = ACCEPTED_ORIGINS)
     @RequestMapping("/instruments")
     public List<Instrument> getInstruments() {
         return MockedDatabase.INSTRUMENTS;
