bd7a40c555127a131423c81405fc9986ab00ce2c
Daniel Porazik <porazik@gmail.com>
2018-09-06 13:43:12 +0200

    Initial Spring Boot setup

---
+++ greenheroes/src/main/java/com/ubs/greenheroes/GreenheroesApplication.java
@@ -0,0 +1,12 @@
+package com.ubs.greenheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenheroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenheroesApplication.class, args);
+	}
+}
