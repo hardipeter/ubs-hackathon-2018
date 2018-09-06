ba4f0b341972546d7acd22732e2a1a705bf6a78b
Peter Hardi <hardipeter@10.34.212.247>
2018-09-06 18:12:33 +0200

    fake data generated

--- greenheroes/src/main/java/com/ubs/greenheroes/generate/MockedDataGenerator.java
+++ greenheroes/src/main/java/com/ubs/greenheroes/generate/MockedDataGenerator.java
@@ -1,7 +1,10 @@
 package com.ubs.greenheroes.generate;
 
 import com.ubs.greenheroes.data.Client;
+import com.ubs.greenheroes.data.ClientPreference;
+import com.ubs.greenheroes.data.ClientPreferenceCategory;
 import com.ubs.greenheroes.data.Instrument;
+import com.ubs.greenheroes.data.InterestLevel;
 import com.ubs.greenheroes.data.MockedDatabase;
 import java.util.Arrays;
 import java.util.List;
@@ -9,7 +12,7 @@ import java.util.Random;
 
 public class MockedDataGenerator {
 
-    private static final List<String> CATEGORY_NAME_LIST = Arrays.asList("Pollution and waste");
+    private static final List<String> CATEGORY_NAME_LIST = Arrays.asList("Pollution and waste", "Climate change", "Water", "People", "Products and services", "Governance", "Ethics");
     private static final Random RANDOM = new Random(0); //0, to always get the same randoms for proper testing
 
     public static void createMockedInstruments() {
@@ -26,6 +29,21 @@ public class MockedDataGenerator {
 
     public static void createMockedClient() {
         Client client = new Client("Luke Skywalker");
+        ClientPreferenceCategory category = new ClientPreferenceCategory("Environmental");
+        category.addPreference(new ClientPreference(CATEGORY_NAME_LIST.get(0), 0, InterestLevel.LOW));
+        category.addPreference(new ClientPreference(CATEGORY_NAME_LIST.get(1), 1, InterestLevel.HIGH));
+        category.addPreference(new ClientPreference(CATEGORY_NAME_LIST.get(2), 2, InterestLevel.LOW));
+        client.addPreferenceCategory(category);
+
+        category = new ClientPreferenceCategory("Social");
+        category.addPreference(new ClientPreference(CATEGORY_NAME_LIST.get(3), 3, InterestLevel.LOW));
+        category.addPreference(new ClientPreference(CATEGORY_NAME_LIST.get(4), 4, InterestLevel.HIGH));
+        client.addPreferenceCategory(category);
+
+        category = new ClientPreferenceCategory("Governance");
+        category.addPreference(new ClientPreference(CATEGORY_NAME_LIST.get(5), 5, InterestLevel.HIGH));
+        category.addPreference(new ClientPreference(CATEGORY_NAME_LIST.get(6), 6, InterestLevel.LOW));
+        client.addPreferenceCategory(category);
         MockedDatabase.CLIENT = client;
     }
 
