61a6daa1f5c5f8b1e77153dece5af56669e95ab8
myklt <maiklasm@gmail.com>
2018-09-07 11:03:28 +0200

    Add score title

--- ui/src/components/PortfolioRating.js
+++ ui/src/components/PortfolioRating.js
@@ -7,6 +7,7 @@ class PortfolioRating extends Component {
     value = value.toFixed(2);
     return (
       <div className="portfolio-rating" style={{ color }}>
+        <h2>Your personal sustainability index score</h2>
         <div className="rating-value">{value}</div>
         <div className={trend > 0 ? 'rating-trend-up' : 'rating-trend-down'} />
         <FiveStar value={value} color={color} />
